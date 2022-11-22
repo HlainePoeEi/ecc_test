4beee5f25acd8351a3c4fb0e8cabefd1332644d4
DESKTOP-D02RKSG\GICPC173 <nyeinmyintzu.kko15@gmail.com>
2022-11-22 14:10:25 +0630

    examList

---
+++ dto/T_YNSExamDto.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'BaseDto.php';
+
+class T_YNSExamDto extends BaseDto
+{
+
+    public $exam_id;
+
+    public $name;
+
+    public $description;
+
+    public $status;
+
+    public $time;
+
+    public $start_date;
+
+    public $end_date;
+
+    public $remarks;
+}
